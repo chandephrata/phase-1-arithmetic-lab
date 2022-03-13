e749878c06e4f37215d4b731df59078e7136b9a5
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-13 14:35:46 -0600

    commit tree 0b5dc21e86e7ea9b9d1a5586577b9c0814ba18ff

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+
+const num1 = 2,
+      num2 = 31,
+      num3 = 16,
+      num4 = 6;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1;
+const mod = num3 % num4;
+const max = Math.max(0, 0.3, 8, 20, 19);
